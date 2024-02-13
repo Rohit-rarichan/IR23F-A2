9352e673ad710b76f408237a6e2451cf0166beb8
Rohit George Rarichan <rraricha@uci.edu>
2024-02-12 23:23:39 -0800

    Writing down the printing and editing fucntions which are to be used after the file has been loaded

--- ui.py
+++ ui.py
@@ -30,6 +30,50 @@ def open_journal(filepath):
     else:
         print("Inalid file format")
 
+def edit_journal(profile, option, open_path):
+    if "-usr" in option:
+        username = option["-usr"]
+        profile.username = username
+    if "-pwd" in option:
+        password = option["-pwd"]
+        profile.password = password
+    if "-bio" in option:
+        bio = option["-bio"]
+        profile.bio = bio 
+    if "-addpost" in option:
+        post_content = option["-addpost"]
+        profile.add_post(post_content)
+    if "-delpost" in option:
+        post_id = option["-delpost"]
+        profile.delete_post(post_id)
+    profile.save_profile(open_path)
+
+def print_journal(profile, option):
+    if "-usr" in option:
+        print("Username: ", profile.username)
+    if "-pwd" in option:
+        print("Password: ", profile.password)
+    if "-bio" in option:
+        print("Bio: ", profile.bio)
+    if "-posts" in option:
+        print("Posts: ")
+        for i, post in enumerate(profile.posts):
+            print(f"ID: {i+1}, Content: {post}")
+    if "-post" in option:
+        post_id = int(option["-post"])
+        if i<= post_id <= len(profile.posts):
+            print(f"Post {post_id} : {profile.posts[post_id - 1]}")
+        else:
+            print("Invalid post ID.")
+    if "-all" in option:
+        print("Username: ", profile.username)
+        print("Password: ", profile.password)
+        print("Bio: ", profile.bio)
+        print("Posts: ")
+        for i, post in enumerate(profile.posts):
+            print(f"ID: {i+1}, Content: {post}")
+
+
 def user_interface():
     while True:
         try:
@@ -44,6 +88,22 @@ def user_interface():
             elif command == 'O':
                 open_path = input("Cool!, Which file would you like to open ")
                 profile = open_journal(open_path)
+            elif command == 'E':
+                keyword = input("Enter the control for what you want to edit ")
+                list_keyword = keyword.split(" ")
+                option = {} 
+                for i in range(0, len(list_keyword), 2):
+                    option[list_keyword[i]] = list_keyword[i+1]
+                edit_journal(profile, option, open_path)
+            elif command == 'P':
+                keyword = input("Enter the control for what you want to output ")
+                list_keyword = keyword.split(" ")
+                option = {} 
+                for i in range(0, len(list_keyword), 2):
+                    list_keyword = list_keyword.strip("/'")
+                    list_keyword = list_keyword.strip('/"')
+                    option[list_keyword[i]] = list_keyword[i+1]
+                print_journal(profile, option)
             elif command == "Q":
                 print("Hope you have completed what you wanted to, Bye!")
                 break
